2a0864205216dc80559aed2a02ecb6a480521537
Andrés F. Murillo <andres.murillo.19@gmail.com>
2016-09-02 19:36:55 -0500

    Update run.sh

--- FlowFence/run.sh
+++ FlowFence/run.sh
@@ -1,1 +1,2 @@
-/root/pox/pox.py log.level --WARNING openflow.of_01 --port=2626 flow_fence_4
+#In the switches use "python application_switch_2.py"
+/root/pox/pox.py log.level --WARNING openflow.of_01 --port=2626 flow_fence_2 forwarding.l2_learning
